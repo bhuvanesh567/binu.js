d52ce18203262f03fc7e5abfee55e77133006a42
PBBinuPrasad <114483084+PBBinuPrasad@users.noreply.github.com>
2023-04-18 19:29:55 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,22 @@
-
+function input()
+{
+  let a=parseInt(prompt("enter the number"));
+  let b=parseInt(prompt("enter the another number"));
+  return [a,b];
+}
+function add(a,b)
+  {
+    return a+b;
+  }
+  function output(a,b,c)
+    {
+      console.log("%d + %d is %d",a,b,c);
+    }
+  function main()
+{
+  let a,b;
+  [a,b]=input();
+  let c = add(a,b);
+  output(a,b,c);
+}
+  main();
