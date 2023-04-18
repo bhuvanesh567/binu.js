16e0df7010b6f117e5308a13d333bc26672d607e
bhuvanesh567 <114483191+bhuvanesh567@users.noreply.github.com>
2023-04-18 20:56:32 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -20,3 +20,27 @@ function add(a,b)
   output(a,b,c);
 }
   main();
+
+
+function input()
+{
+  let a=parseInt(prompt("enter the number"));
+  let b=parseInt(prompt("enter the another number"));
+  return [a,b];
+}
+function multiply(a,b)
+  {
+    return a*b;
+  }
+  function output(a,b,c)
+    {
+      console.log("%d * %d is %d",a,b,c);
+    }
+  function main()
+{
+  let a,b;
+  [a,b]=input();
+  let c = multiply(a,b);
+  output(a,b,c);
+}
+  main();
