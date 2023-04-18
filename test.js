00adef4e6d7d47412b67c87f64ff1448d7d38ff0
bhuvanesh567 <114483191+bhuvanesh567@users.noreply.github.com>
2023-04-18 20:08:33 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
-
+function input()
+{
+  let a=parseInt(prompt("enter the number"));
+  let b=parseInt(prompt("enter the another number"));
+  return [a,b];
+}
+function multiply(a,b)
+  {
+    return a*b;
+  }
+  function output(a,b,c)
+    {
+      console.log("%d * %d is %d",a,b,c);
+    }
+  function main()
+{
+  let a,b;
+  [a,b]=input();
+  let c = multiply(a,b);
+  output(a,b,c);
+}
+  main();
